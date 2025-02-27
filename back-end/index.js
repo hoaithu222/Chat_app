1433fad8255fb9460d46ca0d1d972e773a97d570
Thu <Thu601925@gmail.com>
2025-02-27 10:45:10 +0700

    add

--- back-end/index.js
+++ back-end/index.js
@@ -9,7 +9,6 @@ dotenv.config();
 const app = express();
 app.use(cors({
     origin: "*",
-    credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization",
 }));
