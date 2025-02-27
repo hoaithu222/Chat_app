7cf732ab870733d620e10ce09c01862d6b2f50f6
Thu <Thu601925@gmail.com>
2025-02-27 10:01:08 +0700

    add

--- back-end/index.js
+++ back-end/index.js
@@ -6,7 +6,9 @@ import router from './router/index.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
+
 const app = express();
+
 app.use(cors({
     origin: process.env.FRONTEND_URL || "*",
     credentials: true,
@@ -14,23 +16,20 @@ app.use(cors({
     allowedHeaders: "Content-Type,Authorization",
 }));
 
-
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (request, response) => {
-    response.json({
-        message: "Hello message_chat hoai thu ",
-    });
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api', router);
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("server running at" + PORT);
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello message_chat hoai thu",
     });
 });
+
+// Kết nối DB và export handler thay vì chạy trực tiếp
+export default async function handler(req, res) {
+    await connectDB();
+    return app(req, res);
+}
